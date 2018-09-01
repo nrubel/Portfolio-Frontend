0a367be92018f8f4fa28569dfb71ebaa06945e32
Nasir Uddin <nasir.rubel99@gmail.com>
2018-09-01 14:00:48 +0600

    pagination update

--- src/Pages/SinglePortfolio.jsx
+++ src/Pages/SinglePortfolio.jsx
@@ -106,7 +106,7 @@ class SinglePortfolio extends Component{
                             <div className="row mx-0 project-pagination justify-content-between">
                                 {
                                     this.state.work.prev === 0 ? (<span>&nbsp;</span>) : (
-                                        <a href={'/work/' + this.state.work.prev.ID + '/' + this.state.work.prev.post_name } className="previous-project">
+                                        <a href={`/work/${this.state.work.prev.post_name}`} className="previous-project">
                                             <svg  viewBox="0 0 32.635 32.635"><g><path d="M32.135,16.817H0.5c-0.276,0-0.5-0.224-0.5-0.5s0.224-0.5,0.5-0.5h31.635c0.276,0,0.5,0.224,0.5,0.5S32.411,16.817,32.135,16.817z"/><path d="M13.037,29.353c-0.128,0-0.256-0.049-0.354-0.146L0.146,16.669C0.053,16.575,0,16.448,0,16.315s0.053-0.26,0.146-0.354L12.684,3.429c0.195-0.195,0.512-0.195,0.707,0s0.195,0.512,0,0.707L1.207,16.315l12.184,12.184c0.195,0.195,0.195,0.512,0,0.707C13.293,29.304,13.165,29.353,13.037,29.353z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
                                             <div className="item-details">
                                                 <span className="link-title">Previous Work</span>
@@ -117,7 +117,7 @@ class SinglePortfolio extends Component{
                                 }
                                 {
                                     this.state.work.next === 0 ? (<span>&nbsp;</span>) : (
-                                        <a href={'/work/' + this.state.work.next.ID + '/' + this.state.work.next.post_name } className="next-project">
+                                        <a href={`/work/${this.state.work.next.post_name}`} className="next-project">
                                             <div className="item-details">
                                                 <span className="link-title">Next Work</span>
                                                 <h2 className="link-project-title">{this.state.work.next.post_title}</h2>
